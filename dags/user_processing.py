3f77df47076f7ef74f0c1097782a23e57bcfd0f1
Dev-Kuss <mkuss.dev@gmail.com>
2024-09-09 17:11:48 -0300

    adicionando hook

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -4,6 +4,9 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
 import json
 from pandas import json_normalize
 
@@ -19,6 +22,12 @@ def _process_user(ti):
     })
     process_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
     
+def _store_user():
+    hook = PostgresHook(postgres_conn_id='postgres')
+    hook.copy_expert(
+        sql="COPY users FROM stdin WITH DELIMITER as ','",
+        filename='/tmp/processed_user.csv'
+    )
 
 with DAG (
     'user_processing', start_date=datetime(2024, 9, 9),
@@ -52,3 +61,10 @@ with DAG (
         task_id='process_user',
         python_callable=_process_user
     )
+    
+    _store_user = PythonOperator(
+        task_id='store_user',
+        python_callable=_store_user
+    )
+    
+    extract_user >> process_user
