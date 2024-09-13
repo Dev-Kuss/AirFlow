e167fa81d49405fc650b02a3b8d9ab18898547c8
Dev-Kuss <mkuss.dev@gmail.com>
2024-09-13 14:37:55 -0300

    Retirando exemplos e colocando multiplos DT's para ser trigger

--- dags/consumer.py
+++ dags/consumer.py
@@ -4,11 +4,12 @@ from airflow.decorators import task
 from datetime import datetime
 
 my_file = Dataset("/tmp/my_file.txt")
+my_file_2 = Dataset ("/tmp/my_file_2.txt")
 
 
 with DAG(
     dag_id="consumer",
-    schedule=[my_file],
+    schedule=[my_file, my_file_2],
     start_date=datetime(2024, 1, 1),
     catchup=False
 ): 
